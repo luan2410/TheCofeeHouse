df54b60409342e22d273741adf7e5820182e7fce
Dang Quang Minh <dangquangminh232005@gmail.com>
2025-04-10 13:01:07 +0700

    chinh them file sever

--- be/server.js
+++ be/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 
-=======
 const userRouter = require('./routes/user_router');
 const testRoutes = require('./routes/testRoutes'); ///  import routes vao` . ở đây là import vào "testRoutes" 
 const monNuocRoutes = require('./routes/monNuoc');
->>>>>>> origin/login/register
+
 const cors = require('cors');
 
 // Import routes (chỉ khai báo mỗi cái 1 lần duy nhất)
@@ -15,7 +13,7 @@ const testRoutes = require('./routes/testRoutes');
 const monNuocRoutes = require('./routes/monNuoc');
 const monAnRoutes = require('./routes/monAn');
 const userRouter = require('./routes/user_router');
-const blogRouter = require('./routes/blog'); 
+const blogRouter = require('./routes/blog');
 
 const app = express();
 
